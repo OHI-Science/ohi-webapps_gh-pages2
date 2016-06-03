958a08f67a0d1fb37a3c7615e489e4463491d157
bbest <bbest@nceas.ucsb.edu>
2016-06-03 13:16:15 -0700

    added ohi-shiny2 app to app.Rmd page w/ custom styling https://github.com/OHI-Science/issues/issues/662

---
+++ _site.R
@@ -0,0 +1,19 @@
+# # BHI
+# title       = "OHI Baltic"
+# description = "Ocean Health Index for Baltic"
+# url         = "//ohi-science.org/bhi"
+# baseurl      = "/bhi"
+# study_area  = "Baltic"
+# git_owner   = "OHI-Science"
+# git_repo    = "bhi"
+# app_url     = "http://ohi-science.nceas.ucsb.edu/bhi"
+
+# Global
+title       = "OHI Global"
+description = "Ocean Health Index, Global EEZ"
+url         = "//ohi-science.org/ohi-global"
+baseurl      = "/bhi"
+study_area  = "Global"
+git_owner   = "OHI-Science"
+git_repo    = "ohi-global"
+app_url     = "http://ohi-science.nceas.ucsb.edu/ohi-shiny2"
