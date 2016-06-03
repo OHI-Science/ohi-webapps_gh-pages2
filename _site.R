160b66db56b681525746d4b8ddc7bfcb042c0c05
jules32 <jstewart@nceas.ucsb.edu>
2016-06-03 16:29:44 -0700

    mocking up BHI website further, getting rid of jekyll

--- _site.R
+++ _site.R
@@ -1,19 +1,19 @@
-# # BHI
-# title       = "OHI Baltic"
-# description = "Ocean Health Index for Baltic"
-# url         = "//ohi-science.org/bhi"
-# baseurl      = "/bhi"
-# study_area  = "Baltic"
-# git_owner   = "OHI-Science"
-# git_repo    = "bhi"
-# app_url     = "http://ohi-science.nceas.ucsb.edu/bhi"
-
-# Global
-title       = "OHI Global"
-description = "Ocean Health Index, Global EEZ"
-url         = "//ohi-science.org/ohi-global"
+# BHI
+title       = "Baltic Health Index"
+description = "Ocean Health Index for the Baltic"
+url         = "//ohi-science.org/bhi"
 baseurl      = "/bhi"
-study_area  = "Global"
+study_area  = "the Baltic Sea"
 git_owner   = "OHI-Science"
-git_repo    = "ohi-global"
-app_url     = "http://ohi-science.nceas.ucsb.edu/ohi-shiny2"
+git_repo    = "bhi"
+app_url     = "http://ohi-science.nceas.ucsb.edu/ohi-shiny2" # "http://ohi-science.nceas.ucsb.edu/bhi"
+
+# # Global
+# title       = "OHI Global"
+# description = "Ocean Health Index, Global EEZ"
+# url         = "//ohi-science.org/ohi-global"
+# baseurl      = "/ohi-global"
+# study_area  = "Global"
+# git_owner   = "OHI-Science"
+# git_repo    = "ohi-global"
+# app_url     = "http://ohi-science.nceas.ucsb.edu/ohi-shiny2"
